f608d6686d1c1d84585cf5eb5a70263ab1ffd724
Cody Fritz <clanalia55@gmail.com>
2021-05-28 13:30:31 -0700

    reformat ERD and research into proper SQL setup, preparing for new table entries

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -1,5 +1,14 @@
 package com.qa.ims.persistence.dao;
 
-public class ItemDAO implements Dao<Item> {
+import java.sql.ResultSet;
 
+import org.apache.logging.log4j.LogManager;
+
+public class ItemDAO implements Dao<Item> {
+public static final Logger LOGGER = LogManager.getLogger();
+
+@Override
+public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+	Long id = resultSet.getLong(0)
+}
 }
